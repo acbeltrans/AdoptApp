ff63a1bf1325dfa1abd4c2ee5d5ef706a5eae972
Santiago Serrano <sd.serrano@uniandes.edu.co>
2019-10-20 16:19:16 -0500

    Auto stash before merge of "master" and "origin/master"

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Panel from './Panel.js';
 import InfoElegido from './InfoElegido.js';
 
 class HomeScreen extends React.Component {
+  static navigationOption = {
+    title:'Home',
+  }
   render() {
     return (
       <View>
         <Text>AdoptApp</Text>
-         
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Filter')}
+        />
+      </View>
+    );
+  }
+}
+
+class FilterScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
         {<Panel></Panel>}
         
       </View>
@@ -21,8 +37,8 @@ class HomeScreen extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Filter: Panel,
     Elegido: InfoElegido,
+    Filter: FilterScreen,
   },
   {
     initialRouteName: 'Home',
