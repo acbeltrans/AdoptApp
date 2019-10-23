789bf4eefa0486975cd25f9657c35f093510db21
Santiago Serrano <sd.serrano@uniandes.edu.co>
2019-10-22 19:59:30 -0500

    Arreglo

--- App.js
+++ App.js
@@ -22,79 +22,79 @@ const Users = [
 
 class SwipeScreen extends React.Component {
   static navigationOption = {
-    title:'Home',
+    title: 'Home',
   }
 
-  constructor(){
+  constructor() {
     super()
 
     this.position = new Animated.ValueXY()
 
-    this.state ={
+    this.state = {
       currentIndex: 0
     }
 
     this.rotate = this.position.x.interpolate({
-      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
-      outputRange:['-10deg','0deg','10deg'],
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: ['-10deg', '0deg', '10deg'],
       extrapolate: 'clamp'
     })
 
     this.rotateAndTranslate = {
-      transform:[{
+      transform: [{
         rotate: this.rotate
       },
-      ...this.position.getTranslateTransform() 
-    ]
+      ...this.position.getTranslateTransform()
+      ]
     }
 
     this.likeOpacity = this.position.x.interpolate({
-      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
-      outputRange:[0,0,1],
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [0, 0, 1],
       extrapolate: 'clamp'
     })
 
     this.dislikeOpacity = this.position.x.interpolate({
-      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
-      outputRange:[1,0,0],
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 0],
       extrapolate: 'clamp'
     })
 
     this.nextCardOpacity = this.position.x.interpolate({
-      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
-      outputRange:[1,0,1],
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 1],
       extrapolate: 'clamp'
     })
 
     this.nextCardScale = this.position.x.interpolate({
-      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
-      outputRange:[1,0.8,1],
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0.8, 1],
       extrapolate: 'clamp'
     })
   }
-  componentWillMount(){
+  componentWillMount() {
     this.PanResponder = PanResponder.create({
 
-      onStartShouldSetPanResponder:(etv,gestureState) =>true,
-      onPanResponderMove:(etv,gestureState) => {
+      onStartShouldSetPanResponder: (etv, gestureState) => true,
+      onPanResponderMove: (etv, gestureState) => {
 
-        this.position.setValue({x:gestureState.dx,y: gestureState.dy })
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
       },
 
-      onPanResponderRelease:(etv,gestureState) => {
-        if(gestureState.dx>120){
-          Animated.spring(this.position,{
-            toValue:{x:SCREEN_WIDTH+100,y:gestureState.dy}
-          }).start(()=>{this.setState({currentIndex:this.state.currentIndex+1},()=>{this.position.setValue({x:0,y:0})})})
+      onPanResponderRelease: (etv, gestureState) => {
+        if (gestureState.dx > 120) {
+          Animated.spring(this.position, {
+            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy }
+          }).start(() => { this.setState({ currentIndex: this.state.currentIndex + 1 }, () => { this.position.setValue({ x: 0, y: 0 }) }) })
         }
-        else if(gestureState.dx < -120){
-          Animated.spring(this.position,{
-            toValue:{x:-SCREEN_WIDTH-100,y:gestureState.dy}
-          }).start(()=>{this.setState({currentIndex:this.state.currentIndex+1},()=>{this.position.setValue({x:0,y:0})})})
+        else if (gestureState.dx < -120) {
+          Animated.spring(this.position, {
+            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy }
+          }).start(() => { this.setState({ currentIndex: this.state.currentIndex + 1 }, () => { this.position.setValue({ x: 0, y: 0 }) }) })
         }
-        else{
-          Animated.spring(this.position,{
-            toValue: {x:0,y:0},
+        else {
+          Animated.spring(this.position, {
+            toValue: { x: 0, y: 0 },
             friction: 4
           }).start()
         }
@@ -103,70 +103,70 @@ class SwipeScreen extends React.Component {
     })
   }
 
-  renderUsers = () =>{
+  renderUsers = () => {
 
-    return Users.map((item,i) =>{
+    return Users.map((item, i) => {
 
-      if( i < this.state.currentIndex)
-      {
+      if (i < this.state.currentIndex) {
         return null
       }
-      else if (i == this.state.currentIndex)
-      {
+      else if (i == this.state.currentIndex) {
 
-      return(
-        <Animated.View 
-        {...this.PanResponder.panHandlers}
-          key={item.id} style={[this.rotateAndTranslate,{height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding:10, position:'absolute'}]}>
-          
-          <Animated.View style={{opacity:this.likeOpacity, transform:[{rotate:'-30deg'}], position: 'absolute', top: 50, left: 40,zIndex: 1000}}>
+        return (
+          <Animated.View
+            {...this.PanResponder.panHandlers}
+            key={item.id} style={[this.rotateAndTranslate, { height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
 
-            <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize:32, fontWeight: '800', padding: 10 }}>LIKE</Text>
+            <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
+
+              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
+
+            </Animated.View>
+
+            <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
+
+              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
+
+            </Animated.View>
+
+            <Image style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }} source={item.uri}></Image>
+
+            <Button title="Go to details"
+              onPress={() => this.props.navigation.navigate('Home')} />
 
           </Animated.View>
-
-          <Animated.View style={{opacity:this.dislikeOpacity, transform:[{rotate:'30deg'}], position: 'absolute', top: 50, right: 40,zIndex: 1000}}>
-
-            <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize:32, fontWeight: '800', padding: 10 }}>NOPE</Text>
-
-          </Animated.View>
-            
-          <Image style={{flex:1,height:null,width: null,resizeMode: 'cover', borderRadius:20}} source={item.uri}></Image>
-
-          <Button title="Go to details" 
-            onPress={()=> this.props.navigation.navigate('Home')}/>
-
-        </Animated.View>
-      )
+        )
       }
       else {
         return (
-        <Animated.View 
-          key={item.id} 
-          style={[{opacity:this.nextCardOpacity, transform:[{scale: this.nextCardScale}], 
-          height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding:10, position:'absolute'}]}>
+          <Animated.View
+            key={item.id}
+            style={[{
+              opacity: this.nextCardOpacity, transform: [{ scale: this.nextCardScale }],
+              height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute'
+            }]}>
 
-          <Image style={{flex:1,height:null,width: null,resizeMode: 'cover', borderRadius:20}} source={item.uri}></Image>
+            <Image style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }} source={item.uri}></Image>
 
-        </Animated.View>
+          </Animated.View>
         )
       }
 
     }).reverse()
-    
+
   }
 
   render() {
     return (
-      <View style={{flex:1}}>
-        <View style={{height:60}}>
+      <View style={{ flex: 1 }}>
+        <View style={{ height: 60 }}>
 
         </View>
-        <View style={{flex:1}}>
+        <View style={{ flex: 1 }}>
           {this.renderUsers()}
         </View>
-        <View style={{flex:1}}>
-          
+        <View style={{ flex: 1 }}>
+
         </View>
 
       </View>
@@ -180,7 +180,7 @@ class FilterScreen extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
         {<Panel></Panel>}
-        
+
       </View>
     );
   }
@@ -190,10 +190,10 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View>
-        <Login/>
+        <Login />
         <Text>Details Screen</Text>
-        <Button title="Go to details" 
-        onPress={()=> this.props.navigation.navigate('Swipe')}/>
+        <Button title="Go to details"
+          onPress={() => this.props.navigation.navigate('Swipe')} />
       </View>
     );
   }
@@ -208,7 +208,7 @@ const AppNavigator = createStackNavigator(
 
   },
   {
-    initialRouteName: 'Swipe',
+    initialRouteName: 'Home',
   }
 );
 
